8a48021b025a8147e954b393a9460a90f02a2519
James Young <pronoiac@gmail.com>
2014-09-02 16:43:02 -0700

    Phase IV completed.

--- skeleton/lib/phase4/controller_base.rb
+++ skeleton/lib/phase4/controller_base.rb
@@ -1,16 +1,29 @@
 require_relative '../phase3/controller_base'
 require_relative './session'
+# require "byebug"
 
 module Phase4
   class ControllerBase < Phase3::ControllerBase
+    # overriding from p02: 
+    # Set the response status code and header
     def redirect_to(url)
+      super(url)
+      session.store_session(res)
     end
 
+    # overriding from p03
     def render_content(content, type)
+      super(content, type)
+      session.store_session(res)
     end
 
     # method exposing a `Session` object
     def session
+      @session ||= Session.new(@req)
     end
+    
+    
+    
+    
   end
 end
--- skeleton/lib/phase4/session.rb
+++ skeleton/lib/phase4/session.rb
@@ -1,22 +1,45 @@
 require 'json'
 require 'webrick'
+# require 'byebug'
 
 module Phase4
   class Session
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      @session = {}
+      cookie_spotted = false
+      unless req.nil?
+        req.cookies.each do |cookie|
+          if cookie.name == "_rails_lite_app"
+            if $debug # || true
+              puts "cookie found!"
+              p cookie.name
+              p cookie.value
+            end
+            @session = JSON.parse(cookie.value)
+            cookie_spotted = true
+          end
+        end
+      end      
+      @session["temp"] = "1st" unless cookie_spotted 
     end
 
     def [](key)
+      @session[key]
     end
 
     def []=(key, val)
+      @session[key] = val
     end
-
+    
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      addl = WEBrick::Cookie.new("_rails_lite_app", JSON.generate(@session))
+      res.cookies << addl
+      # puts "debug. addl: "
+      # p addl
     end
   end
 end
