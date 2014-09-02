9b07b1d3d3219085491a2a74e67ec91614da3bf6
James Young <pronoiac@gmail.com>
2014-09-02 14:34:07 -0700

    Phase III completed.

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -1,12 +1,37 @@
 require_relative '../phase2/controller_base'
 require 'active_support/core_ext'
 require 'erb'
+# require 'byebug'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
-    def render(template_name)
+    
+    attr_reader :req, :res
+
+    # Setup the controller
+    def initialize(req, res)
+      @req = req
+      @res = res
+      @already_built_response = false
     end
+    
+    def already_built_response?
+      @already_built_response
+    end
+    
+    def render(template_name)
+      raise if already_built_response?
+      # @already_built_response = true
+      
+      # file: views/#{controller_name}/#{template_name}.html.erb).
+      controller_name = self.class.to_s.underscore # e.g. my_controller
+      # e.g. template_name = show.html.erb
+      read_template = File.read "views/#{controller_name}/#{template_name}.html.erb"
+      processed = ERB.new(read_template).result(binding) 
+      render_content(processed, "text/html")
+    end
+
   end
 end
