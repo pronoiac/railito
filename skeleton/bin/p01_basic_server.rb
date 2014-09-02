d904ea06b6fdee869127023d2311ae0755dda6de
James Young <pronoiac@gmail.com>
2014-09-02 13:24:08 -0700

    Phase I complete.

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -4,3 +4,15 @@ require 'webrick'
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
+
+# main
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+trap('INT') { server.shutdown } # gracefully handle ctrl-c
+
+server.mount_proc "/" do |req, res| # request, response
+  content_type=("text/text")
+  res.body = req.path
+end
+
+server.start
