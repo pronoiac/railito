b70d4d4bfce11718c542a5450aeb59e19fc2d49f
James Young <pronoiac@gmail.com>
2014-09-03 16:23:33 -0700

    Phase VI.b nearly completed - Router written

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -45,7 +45,7 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
-      define_method(http_method) do
+      define_method(http_method) do |pattern, controller_class, action_name|
         add_route(pattern, http_method, controller_class, action_name)
       end
     end
