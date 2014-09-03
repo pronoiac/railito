accdd02b8d76cce89add2ee3bf443b1333175d4f
James Young <pronoiac@gmail.com>
2014-09-03 16:13:15 -0700

    Phase VI.b nearly done - Router written, except methods for HTTP methods

--- skeleton/lib/phase6/router.rb
+++ skeleton/lib/phase6/router.rb
@@ -6,7 +6,8 @@ module Phase6
 
     def initialize(pattern, http_method, controller_class, action_name)
       @pattern = pattern
-      @http_method = http_method.downcase.to_sym
+      @http_method = http_method.to_s.downcase.to_sym
+        # the #to_s is because of a spec using *numbers*
       @controller_class = controller_class
       @action_name = action_name
     end
@@ -28,10 +29,12 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
@@ -42,10 +45,17 @@ module Phase6
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      @routes.each do |route|
+        return route if route.matches?(req)
+      end
+      nil
     end
 
     # either throw 404 or call run on a matched route
